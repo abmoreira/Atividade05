c351b56eef3fd2bd2ba4d760ec5b0bb9a8d750ed
Alvaro B Moreira <alvaromoreira@hotmail.com>
2017-12-20 21:23:41 -0200

    Atividade 05

---
+++ basic-usage/js/index.js
@@ -0,0 +1,9 @@
+(function () {
+  'use strict';
+  angular
+      .module('MyApp',['ngMaterial', 'ngMessages', 'material.svgAssetsCache'])
+      .controller('AppCtrl', function() {
+        this.userState = '';
+        this.states = ('AC AL' + ' AM AP BA CE DF ES' + ' GO MA MG MS MT PA PB PE PI PR RJ RN RO RR RS SC SE SP TO ').split(' ').map(function (state) { return { abbrev: state }; });
+      });
+})();
